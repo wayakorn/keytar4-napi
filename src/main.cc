2ef8f1cca73de26150af94fc59e745b64506eb26
Way Vadhanasin <wayvad@microsoft.com>
2020-07-12 21:50:23 -0700

    keytar4-napi version 4.1.1

---
+++ src/main.cc
@@ -0,0 +1,159 @@
+#include "napi.h"
+#include "async.h"
+
+namespace {
+
+Napi::Value SetPassword(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+
+  if (!info[0].IsString()) {
+    Napi::TypeError::New(env, "Parameter 'service' must be a string").
+      ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  std::string service = info[0].As<Napi::String>();
+
+  if (!info[1].IsString()) {
+    Napi::TypeError::New(env, "Parameter 'username' must be a string").
+      ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  std::string username = info[1].As<Napi::String>();
+
+  if (!info[2].IsString()) {
+    Napi::TypeError::New(env, "Parameter 'password' must be a string").
+      ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  std::string password = info[2].As<Napi::String>();
+  std::string targetname = info[3].As<Napi::String>();
+
+  if (!info[4].IsNumber()) {
+    Napi::TypeError::New(env, "Parameter 'credType' must be a number").
+      ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  const int credType = info[4].As<Napi::Number>();
+
+  if (!info[5].IsNumber()) {
+    Napi::TypeError::New(env, "Parameter 'credPersist' must be a number").
+      ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  const int credPersist = info[5].As<Napi::Number>();
+
+  SetPasswordWorker* worker = new SetPasswordWorker(
+    service,
+    username,
+    password,
+    targetname,
+    credType,
+    credPersist,
+    env);
+  worker->Queue();
+  return worker->Promise();
+}
+
+Napi::Value GetPassword(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  if (!info[0].IsString()) {
+    Napi::TypeError::New(env, "Parameter 'service' must be a string").
+      ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  std::string service = info[0].As<Napi::String>();
+
+  if (!info[1].IsString()) {
+    Napi::TypeError::New(env, "Parameter 'username' must be a string").
+      ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  std::string username = info[1].As<Napi::String>();
+
+  GetPasswordWorker* worker = new GetPasswordWorker(
+    service,
+    username,
+    env);
+  worker->Queue();
+  return worker->Promise();
+}
+
+Napi::Value DeletePassword(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  if (!info[0].IsString()) {
+    Napi::TypeError::New(env, "Parameter 'service' must be a string").
+      ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  std::string service = info[0].As<Napi::String>();
+
+  if (!info[1].IsString()) {
+    Napi::TypeError::New(env, "Parameter 'username' must be a string").
+      ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  std::string username = info[1].As<Napi::String>();
+
+  DeletePasswordWorker *worker = new DeletePasswordWorker(
+    service,
+    username,
+    env);
+  worker->Queue();
+  return worker->Promise();
+}
+
+Napi::Value FindPassword(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  if (!info[0].IsString()) {
+    Napi::TypeError::New(env, "Parameter 'service' must be a string").
+      ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  std::string service = info[0].As<Napi::String>();
+
+  FindPasswordWorker* worker = new FindPasswordWorker(
+    service,
+    env);
+  worker->Queue();
+  return worker->Promise();
+}
+
+Napi::Value FindCredentials(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  if (!info[0].IsString()) {
+    Napi::TypeError::New(env, "Parameter 'service' must be a string").
+      ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  std::string service = info[0].As<Napi::String>();
+
+  FindCredentialsWorker* worker = new FindCredentialsWorker(
+    service,
+    env);
+  worker->Queue();
+  return worker->Promise();
+}
+
+Napi::Object Init(Napi::Env env, Napi::Object exports) {
+  exports.Set("getPassword", Napi::Function::New(env, GetPassword));
+  exports.Set("setPassword", Napi::Function::New(env, SetPassword));
+  exports.Set("deletePassword", Napi::Function::New(env, DeletePassword));
+  exports.Set("findPassword", Napi::Function::New(env, FindPassword));
+  exports.Set("findCredentials", Napi::Function::New(env, FindCredentials));
+  return exports;
+}
+
+}  // namespace
+
+NODE_API_MODULE(keytar, Init)
